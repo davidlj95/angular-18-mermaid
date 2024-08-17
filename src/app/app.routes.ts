ddee63a2b198172b8ef48e2fd5cdf9cac43734d6
David LJ <mail@davidlj95.com>
2024-08-17 19:09:11 +0200

    feat: generate mermaid component
    ng g c mermaid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MermaidComponent } from "./mermaid/mermaid.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '', pathMatch: 'full', component: MermaidComponent
+}];
---
+++ src/app/mermaid/mermaid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mermaid',
+  standalone: true,
+  imports: [],
+  templateUrl: './mermaid.component.html',
+  styleUrl: './mermaid.component.css'
+})
+export class MermaidComponent {
+
+}
